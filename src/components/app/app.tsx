1a92efc629d21f2ee8f2ba0644b447b9eec78bc3
apykhanov <121295115+apykhanov@users.noreply.github.com>
2024-11-20 18:30:36 +0300

    Начинаем программировать

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import Main from "../../pages/main/main.tsx";
+
+
+type AppProps = {
+  cardCount: number;
+}
+
+export default function App({ cardCount }: AppProps) {
+
+  return (
+    <Main cardCount={cardCount} />
+  )
+}
+
