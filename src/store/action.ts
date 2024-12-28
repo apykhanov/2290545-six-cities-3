eb205074f2eb039444359c2aad2b44944ffd0771
apykhanov <121295115+apykhanov@users.noreply.github.com>
2024-12-28 22:03:18 +0300

    Контроль и ограничения(часть2) (#8)

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,7 @@
 import {createAction} from '@reduxjs/toolkit';
 import {OfferPreview} from '../types/offer.ts';
+import {Sorting} from '../types/sort.ts';
 
 export const setCity = createAction<string>('city/setCity');
 export const getOffers = createAction<OfferPreview[]>('offers/getOffers');
+export const changeSortingTypes = createAction<Sorting>('offers/changeSortingTypes');
