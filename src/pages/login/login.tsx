2bcb05690f06bca06fc79899634766c62094e093
apykhanov <121295115+apykhanov@users.noreply.github.com>
2025-01-19 15:37:34 +0300

    Истина где-то на сервере (#10)

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,14 +1,43 @@
+import {FormEvent, useRef} from 'react';
+import {useAppDispatch} from '../../hook/use-app-dispatch.tsx';
+import {useNavigate} from 'react-router-dom';
+import {loginAction} from '../../store/api-actions.ts';
+import {AppRoute} from '../../const.ts';
+
+
 export default function Login() {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      dispatch(loginAction({
+        login: loginRef.current.value,
+        password: passwordRef.current.value.trim()
+      }));
+    }
+  };
 
   return (
     <main className="page__main page__main--login">
       <div className="page__login-container container">
         <section className="login">
           <h1 className="login__title">Sign in</h1>
-          <form className="login__form form" action="#" method="post">
+          <form
+            className="login__form form"
+            action="#"
+            method="post"
+            onSubmit={handleSubmit}
+          >
             <div className="login__input-wrapper form__input-wrapper">
               <label className="visually-hidden">E-mail</label>
               <input
+                ref={loginRef}
                 className="login__input form__input"
                 type="email"
                 name="email"
@@ -19,6 +48,7 @@ export default function Login() {
             <div className="login__input-wrapper form__input-wrapper">
               <label className="visually-hidden">Password</label>
               <input
+                ref={passwordRef}
                 className="login__input form__input"
                 type="password"
                 name="password"
@@ -26,7 +56,9 @@ export default function Login() {
                 // required=""
               />
             </div>
-            <button className="login__submit form__submit button" type="submit">
+            <button className="login__submit form__submit button" type="submit"
+              onClick={() => navigate(AppRoute.Root)}
+            >
               Sign in
             </button>
           </form>
