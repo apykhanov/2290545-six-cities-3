f423a0b37522b94d39634985dcfcc3529406b32f
apykhanov <121295115+apykhanov@users.noreply.github.com>
2025-02-04 22:14:28 +0300

    Оптимизация производительности 2

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -19,16 +19,22 @@ export default function Favorites() {
 
   if (favorites.length === 0) {
     return (
-      <div className="page">
+      <div className="page page--favorites-empty">
         <header className="header">
           <div className="container">
             <Header />
           </div>
         </header>
-        <main className="page__main page__main--favorites">
+        <main className="page__main page__main--favorites page__main--favorites-empty">
           <div className="page__favorites-container container">
-            <section className="favorites">
-              <h1 className="favorites__title">No favorites found</h1>
+            <section className="favorites favorites--empty">
+              <h1 className="visually-hidden">Favorites (empty)</h1>
+              <div className="favorites__status-wrapper">
+                <b className="favorites__status">Nothing yet saved.</b>
+                <p className="favorites__status-description">Save properties to narrow down search or plan your future
+                  trips.
+                </p>
+              </div>
             </section>
           </div>
         </main>
